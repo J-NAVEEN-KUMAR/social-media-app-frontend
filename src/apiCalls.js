33aeeaf401d21525e69e33e5733786d9caf81c45
J-NAVEEN-KUMAR <naveen.jeruban@gmail.com>
2022-05-14 10:37:13 +0530

    added heroku server

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -4,7 +4,10 @@ import { toast } from "react-toastify";
 export const loginCall = async (userCredentials, dispatch) => {
   dispatch({ type: "LOGIN_START" });
   try {
-    const res = await axios.post("auth/login", userCredentials);
+    const res = await axios.post(
+      `${process.env.REACT_APP_API}/auth/login`,
+      userCredentials
+    );
     const { token, user } = res.data;
     window.localStorage.setItem("token", JSON.stringify(token));
     window.localStorage.setItem("user", JSON.stringify(user));
